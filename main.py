3d4e9921f121bbb7c53f44da0dfc7463152a43ec
shariquereyaz <45137091+shariquereyaz@users.noreply.github.com>
2022-09-18 16:55:50 +0000

    10

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-sentence = input()
-num_words = len(sentence)
-print(num_words)
+dept    = input()
+course  = input()
+prefix  = input()
+name    = input()
+roll_no = input()
+name = prefix + " " + name.upper() # one space
+lib_id = dept[0] + course[0] + roll_no
+print("Student record:")
+indent = '    '	# four spaces
+print(indent + "Dept:", dept)
+print(indent + "Name:", name)
+print(indent + "Roll No:", roll_no)
+print(indent + "Library Card No:", lib_id)
