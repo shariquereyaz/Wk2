77d809b97ed96a08550023580ad8dfd7598b2e82
shariquereyaz <45137091+shariquereyaz@users.noreply.github.com>
2022-09-18 13:47:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#Dynaming typing
+
+a=10
+print(type(a))
+
+a='India'
+print(type(a))
+n=10
+print(type(n))
+print(n)
+n=n/2
+print(n)
+print(type(n))
+x,y=1,2
+print(y,x)
+a=b=c=10
+print(a,b,c)
+s=100
+print(s)
+del s
+#print(s)
+print('abc' in 'dskjfssdsdabc')
+print('xyz' in 'dskjfssdsdabc')
+z=''''hehhe
+locals
+dlsf'''
+print(z)
+
