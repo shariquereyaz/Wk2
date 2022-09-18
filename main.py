8c10a0bd92d933f006a918be1f44ee188d3736b4
shariquereyaz <45137091+shariquereyaz@users.noreply.github.com>
2022-09-18 15:22:53 +0000

    Import statement

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-alpha='abcdefghijklmnopqrstuvwxyz'
-s='sharique'
-t=''
-i=0
-k=1
-t=t+(alpha[(((alpha.index(s[i]))+k)%26)])
-print(alpha.index(s[i]))
-#t=t+(alpha[(((alpha.index(s[i+1]))+k)%26)])
-#print(t)
+import calendar
 
-#t=t+(alpha[(((alpha.index(s[i+2]))+k)%26)])
-#t=t+(alpha[(((alpha.index(s[i+3]))+k)%26)])
-#t=t+(alpha[(((alpha.index(s[i+4]))+k)%26)])
-#t=t+(alpha[(((alpha.index(s[i+5]))+k)%26)])
-#t=t+(alpha[(((alpha.index(s[i+6]))+k)%26)])
-#t=t+(alpha[(((alpha.index(s[i+7]))+k)%26)])
-#t=t+(alpha[(((alpha.index(s[i+8]))+k)%26)])
-
-print(t)
+print(calendar.month(2022,9))
+print(calendar.calendar(2022))
