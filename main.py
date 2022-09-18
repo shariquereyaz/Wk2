bb4ab6c0bdc382574f98a9ccb740ffdd61001128
shariquereyaz <45137091+shariquereyaz@users.noreply.github.com>
2022-09-18 16:53:13 +0000

    9

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-x = int(input())
-y = int(input())
-z = int(input())
-
-# Block-1 Start
-if x > 0 or y > 0 or z > 0:
-    if (x > 0 and y > 0) or (y > 0 and z > 0) or (z > 0 and x > 0):
-        if x > 0 and y > 0 and z > 0:
-            print('P3')
-            print(10)
-        else:
-            print('P2')
-            print(13)
-    else:
-        print('P1')
-        print(16)
-# Block-1 End
-
-# Block-2 Start
-if x < 0 or y < 0 or z < 0:
-    if (x < 0 and y < 0) or (y < 0 and z < 0) or (z < 0 and x < 0):
-        if x < 0 and y < 0 and z < 0:
-            print('N3')
-            print(24)
-        else:
-            print('N2')
-            print(27)
-    else:
-        print('N1')
-        print(30)
-# Block-2 End
+sentence = input()
+num_words = len(sentence)
+print(num_words)
