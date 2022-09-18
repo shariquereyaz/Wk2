5b992cef4549acac6735c6ae42a1a79029a58df1
shariquereyaz <45137091+shariquereyaz@users.noreply.github.com>
2022-09-18 16:50:58 +0000

    5 6 7 8

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-num = int(input())
-first, middle, last = int(num[0]), int(num[1]), int(num[2])
-if first + last == middle:
-    print('sandwich')
-else:
-    print('plain')
+x = int(input())
+y = int(input())
+z = int(input())
+
+# Block-1 Start
+if x > 0 or y > 0 or z > 0:
+    if (x > 0 and y > 0) or (y > 0 and z > 0) or (z > 0 and x > 0):
+        if x > 0 and y > 0 and z > 0:
+            print('P3')
+            print(10)
+        else:
+            print('P2')
+            print(13)
+    else:
+        print('P1')
+        print(16)
+# Block-1 End
+
+# Block-2 Start
+if x < 0 or y < 0 or z < 0:
+    if (x < 0 and y < 0) or (y < 0 and z < 0) or (z < 0 and x < 0):
+        if x < 0 and y < 0 and z < 0:
+            print('N3')
+            print(24)
+        else:
+            print('N2')
+            print(27)
+    else:
+        print('N1')
+        print(30)
+# Block-2 End
